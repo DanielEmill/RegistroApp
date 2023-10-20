f1f2da5554c5ed9cebc50c481f0d84adce20f9e7
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-10-19 20:31:19 -0400

    update

---
+++ app/src/main/java/com/example/registroapp/ComposeApp.kt
@@ -0,0 +1,5 @@
+package com.example.registroapp
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+@HiltAndroidApp
+class ComposeApp: Application(){}
---
+++ app/src/main/java/com/example/registroapp/di/AppModule.kt
@@ -0,0 +1,37 @@
+package com.example.registroapp.di
+import com.example.registroapp.data.remote.dto.ClientesApi
+import com.example.registroapp.data.repository.ClienteRepository
+import com.example.registroapp.data.repository.ClienteRepositoryImp
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    abstract fun bindOcupacionRepository(impl: ClienteRepositoryImp): ClienteRepository
+}
+
+@Singleton
+@Provides
+fun providesMoshi(): Moshi {
+    return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+}
+
+@Singleton
+@Provides
+fun providesMiClientesApi(moshi: Moshi): ClientesApi {
+    return Retrofit.Builder().baseUrl("https://miclientesapi.azurewebsites.net")
+        .addConverterFactory(MoshiConverterFactory.create(moshi)).build()
+        .create(ClientesApi::class.java)
+}
+
